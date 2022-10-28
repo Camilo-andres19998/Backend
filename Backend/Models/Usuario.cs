594274ade5eca1a74ec51b93c0c875d01898e59d
Camilo-andres19998 <andrescamilopoblete@gmail.com>
2022-10-28 19:17:38 -0300

    Crear base de datos temporal

--- Backend/Models/Usuario.cs
+++ Backend/Models/Usuario.cs
@@ -2,10 +2,58 @@
 {
     public class Usuario
     {
+        public string idUsuario { get; set; }
+        public string usuario { get; set; }
+
+        public string passrowd { get; set; }
+
+        public string rol { get; set; }
+
+        public static List<Usuario> DB()
+        {
+            var list = new List<Usuario>()
+            {
+                new Usuario
+                {
+                    idUsuario = "1",
+                    usuario = "Camilo",
+                    passrowd = "123.",
+                    rol = "empleado"
+                },
+
+                 new Usuario
+                {
+                    idUsuario = "1",
+                    usuario = "Daniel",
+                    passrowd = "123.",
+                    rol = "empleado"
+                },
+
+                   new Usuario
+                {
+                    idUsuario = "2",
+                    usuario = "Benjamin",
+                    passrowd = "123.",
+                    rol = "asesor"
+                },
+
+
+                     new Usuario
+                {
+                    idUsuario = "3",
+                    usuario = "Alejandro",
+                    passrowd = "123.",
+                    rol = "administrador"
+                },
+
+
+            };
+
+            return list;
 
 
 
-
-
+        }
     }
+
 }
