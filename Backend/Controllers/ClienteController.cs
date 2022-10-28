b4cc9d2e797535d2dd71a8b3e4d9f76149b4162d
Camilo-andres19998 <andrescamilopoblete@gmail.com>
2022-10-28 16:56:25 -0300

    Creacion de proyecto

---
+++ Backend/Controllers/ClienteController.cs
@@ -0,0 +1,42 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+
+    [ApiController]
+    [Route("cliente")]
+    public class ClienteController : ControllerBase
+    {
+
+
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente()
+        {
+            List<Cliente> clientes = new List<Cliente>
+
+            {
+                new Cliente
+                {
+                    id = "1",
+                    nombre = "Camilo",
+                    descripcion = "Cliente con tarea resuelta"
+                },
+
+                new Cliente
+                {
+                    id = "2",
+                    nombre = "Benjamin",
+                    descripcion = "Cliente con tarea resuelta"
+                }
+
+                };
+
+            return clientes;
+        }
+
+    }
+
+}
+
---
+++ Backend/Models/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models
+{
+    public class Cliente
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+    }
+}
